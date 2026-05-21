5354b69983075847e1ef6067e080aa6c584f1aec
AshrafGad1001 <ashrafgad542@gmail.com>
2026-05-21 23:35:34 +0300

    install Packages

---
+++ src/Components/Header/HeaderDown.jsx
@@ -0,0 +1,9 @@
+
+
+function HeaderDown() {
+    return (
+        <div>HeaderDown</div>
+    )
+}
+
+export default HeaderDown
---
+++ src/Components/Header/HeaderUp.jsx
@@ -0,0 +1,10 @@
+
+function HeaderUp() {
+    return (
+        <div className="header-up">
+            <div className="container"></div>
+        </div>
+    )
+}
+
+export default HeaderUp
